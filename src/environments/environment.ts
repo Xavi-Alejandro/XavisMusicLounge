80d96df9076213422aebcb575238ce94fb6f336a
Xavi-Alejando <79874223+Xavi-Alejando@users.noreply.github.com>
2024-02-12 19:04:21 -0500

    Back up and running with digital ocean

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,9 +4,10 @@
 
 export const environment = {
   production: false,
-  clientID: "20abc4ff00334dd28c87446397ffa7e1",
-  clientSecret: "2975faa96e034feeb6e80b7b3163aac3",
-  userAPIBase: "https://assignment-6-spotify-users.herokuapp.com/api/user"
+  clientID: '20abc4ff00334dd28c87446397ffa7e1',
+  clientSecret: '2975faa96e034feeb6e80b7b3163aac3',
+  //userAPIBase: "https://assignment-6-spotify-users.herokuapp.com/api/user"
+  userAPIBase: 'https://shark-app-5moaj.ondigitalocean.app/api/user',
 };
 
 /*
